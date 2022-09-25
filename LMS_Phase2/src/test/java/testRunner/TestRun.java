14238373ebacc4605c85c9fb2e2de306796fa82e
santh <santh@DESKTOP-8CG10D5.home>
2022-09-25 17:37:40 -0400

    running all features - Ramya

--- LMS_Phase2/src/test/java/testRunner/TestRun.java
+++ LMS_Phase2/src/test/java/testRunner/TestRun.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions
        (
-		features=".//Features/Logout.feature",
+		features=".//Features",
 		glue= "stepDefinitions",
 		dryRun=false,
 		monochrome = true,
